27673389c90387dc8a6ac7619772b68ea0f2aa1d
Luan <luan.ag2012@hotmail.com>
2024-04-06 22:11:34 -0300

    first commit

---
+++ cursoLaravel/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
