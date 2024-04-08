da17c4ed10479fa923899c10c9752f6ab824f86d
luangoularte <luan.ag2012@hotmail.com>
2024-04-08 11:00:27 -0300

    first commit

--- cursoLaravelLinux/resources/views/welcome.blade.php
+++ cursoLaravelLinux/resources/views/welcome.blade.php
@@ -6,12 +6,17 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="/" method="GET">
+    <form action="/" method="GET" class="form-welcome">
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+        <input type="submit" class="btn btn-primary" value="Pesquisar">
     </form>
 </div>
 <div id="events-container" class="col-md-12">
+    @if ($search)
+    <h2>Buscando por {{ $search }}</h2>
+    @else
     <h2>Próximos Eventos</h2>
+    @endif
     <p class="subtitle">Veja os eventos dos próximos dias</p>
     <div id="cards-container" class="row">
         @foreach($events as $event)
@@ -26,10 +31,12 @@
         </div>
         @endforeach
 
-        @if(count($events) == 0)
+        @if(count($events) == 0 && $search)
+            <p>Não foi possível encontar nenhum evento com "{{ $search }}". <a href="/">Ver todos</a></p>
+        @elseif (count($events) ==0)
             <p>Não há eventos disponíveis</p>
         @endif
-    </div>
+    </div>  
 </div>
 
 @endsection
