cd456451834e422fddf53596a0f4a6af97d27c2b
Luan <116608514+luangoularte@users.noreply.github.com>
2025-08-20 09:13:54 -0300

    Delete DDD1 directory

--- DDD1/app/App/Core/Http/Controllers/Controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Core\Http\Controllers;
-
-abstract class Controller
-{
-    //
-}
--- DDD1/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::namespace('\App\Web\Task\Controllers')->group( function () {
-    Route::get('/', 'TaskController@index');
-    Route::post('/', 'TaskController@store');
-
-});
